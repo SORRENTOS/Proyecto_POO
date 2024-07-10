1f3e9297e052250135abca175dbc6e31156976dc
Zafika22 <cristian.pizarrov@usm.cl>
2024-07-10 18:01:45 -0400

    Posibilidad de visualizar Tablas en ui

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+//import javafx.scene.chart.PieChart.Data;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -9,6 +10,9 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         try {
+            Database database = new Database();
+            database.createTablesIfNotExist();
+
             Parent root = FXMLLoader.load(getClass().getResource("Scene_StartUp.fxml"));
             Scene scene = new Scene(root);
             stage.setScene(scene);
--- src/SceneController.java
+++ src/SceneController.java
@@ -1,42 +1,102 @@
-import java.io.IOException;
-
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 
 public class SceneController {
     private Stage stage;
     private Scene scene;
     private Parent root;
 
-    public void switchToScene_Books(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("Scene_Books.fxml"));
+    @FXML
+    private TableView<Documento> TablaDocumentos;
+    @FXML
+    private TableColumn<Documento, String> TituloDocs;
+    @FXML
+    private TableColumn<Documento, String> PublisherDocs;
+    @FXML
+    private TableColumn<Documento, Integer> PagDocs;
+    @FXML
+    private TableColumn<Documento, Integer> CantDocs; 
+    
+    @FXML
+    private TableView<Libro> TablaBook;
+    @FXML
+    private TableColumn<Libro, String> TituloBook;
+    @FXML
+    private TableColumn<Libro, String> AuthorBook;
+    @FXML
+    private TableColumn<Libro, Integer> PagBook;
+    @FXML
+    private TableColumn<Libro, Integer> CantBook;
+
+    @FXML
+    private TableView<Revista> TablaMag;
+    @FXML
+    private TableColumn<Revista, String> TituloMag;
+    @FXML
+    private TableColumn<Revista, Integer> PagMag;
+    @FXML
+    private TableColumn<Revista, Integer> CantMag;
+    @FXML
+    private TableColumn<Revista, Integer> DescMag;
+
+    private Database database = new Database();
+
+    public void switchToScene_Init(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Scene_StartUp.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void switchToScene_Books(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Scene_Books.fxml"));
+        root = loader.load();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+        SceneController controller = loader.getController();
+        controller.initializeBookScene();;
     }
 
     public void switchToScene_Docs(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("Scene_Docs.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Scene_Docs.fxml"));
+        root = loader.load();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+
+        SceneController controller = loader.getController();
+        controller.initializeDocsScene();
     }
 
     public void switchToScene_Magazines(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("Scene_Magazines.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Scene_Magazines.fxml"));
+        root = loader.load();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+
+        SceneController controller = loader.getController();
+        controller.initializeMagScene();
     }
     public void closeWindow(ActionEvent event)throws IOException{
         Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
@@ -47,13 +107,14 @@ public class SceneController {
     Platform.exit();
     }
 
-    public void Add_Book(ActionEvent event )throws IOException {
+    public void Add_Book(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("Scene_AddBook.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Stage popupStage = new Stage();
         scene = new Scene(root);
-        stage.setScene(scene);
-        stage.initModality(Modality.APPLICATION_MODAL);
-        stage.showAndWait();
+        popupStage.setScene(scene);
+        popupStage.initModality(Modality.APPLICATION_MODAL);
+        popupStage.initOwner(((Node) event.getSource()).getScene().getWindow());
+        popupStage.showAndWait();
     }
 
     public void confirmAdd_Book(ActionEvent event){
@@ -61,26 +122,28 @@ public class SceneController {
         stage.close();
     }
 
-    public void Add_Doc(ActionEvent event )throws IOException {
+    public void Add_Doc(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("Scene_AddDoc.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Stage popupStage = new Stage();
         scene = new Scene(root);
-        stage.setScene(scene);
-        stage.initModality(Modality.APPLICATION_MODAL);
-        stage.showAndWait();
+        popupStage.setScene(scene);
+        popupStage.initModality(Modality.APPLICATION_MODAL);
+        popupStage.initOwner(((Node) event.getSource()).getScene().getWindow());
+        popupStage.showAndWait();
     }
     
     public void confirmAdd_Doc(ActionEvent event){
         Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         stage.close();
     }
-    public void Add_Mag(ActionEvent event )throws IOException {
-        root = FXMLLoader.load(getClass().getResource("Scene_AddDoc.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    public void Add_Mag(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Scene_AddMag.fxml")); 
+        Stage popupStage = new Stage();
         scene = new Scene(root);
-        stage.setScene(scene);
-        stage.initModality(Modality.APPLICATION_MODAL);
-        stage.showAndWait();
+        popupStage.setScene(scene);
+        popupStage.initModality(Modality.APPLICATION_MODAL);
+        popupStage.initOwner(((Node) event.getSource()).getScene().getWindow());
+        popupStage.showAndWait();
     }
     
     public void confirmAdd_Mag(ActionEvent event){
@@ -88,5 +151,32 @@ public class SceneController {
         stage.close();
     }
 
+    private void initializeDocsScene() {
+        TituloDocs.setCellValueFactory(new PropertyValueFactory<>("titulo"));
+        PublisherDocs.setCellValueFactory(new PropertyValueFactory<>("publicador"));
+        PagDocs.setCellValueFactory(new PropertyValueFactory<>("numPaginas"));
+        CantDocs.setCellValueFactory(new PropertyValueFactory<>("cantidadDisp"));
+
+        ObservableList<Documento> documentos = FXCollections.observableArrayList(database.getAllDocumentos());
+        TablaDocumentos.setItems(documentos);
+    }
+    private void initializeBookScene() {
+        TituloBook.setCellValueFactory(new PropertyValueFactory<>("titulo"));
+        AuthorBook.setCellValueFactory(new PropertyValueFactory<>("autor"));
+        PagBook.setCellValueFactory(new PropertyValueFactory<>("numPaginas"));
+        CantBook.setCellValueFactory(new PropertyValueFactory<>("cantidadDisp"));
+
+        ObservableList<Libro> libros = FXCollections.observableArrayList(database.getAllLibros());
+        TablaBook.setItems(libros);;
+    }
+    private void initializeMagScene() {
+        TituloMag.setCellValueFactory(new PropertyValueFactory<>("titulo"));
+        PagMag.setCellValueFactory(new PropertyValueFactory<>("numPaginas"));
+        CantMag.setCellValueFactory(new PropertyValueFactory<>("cantidadDisp"));
+        DescMag.setCellValueFactory(new PropertyValueFactory<>("descripcion"));
+
+        ObservableList<Revista> revistas = FXCollections.observableArrayList(database.getAllRevistas());
+        TablaMag.setItems(revistas);
+    }
 
 }
