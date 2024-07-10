8b5a6e578785901fa50d24a2b025a383f532b2a4
Zafika22 <cristian.pizarrov@usm.cl>
2024-07-10 01:29:06 -0400

    Creacion escenas para añadir cada tipo de documento

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("Scene_Books.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("Scene_StartUp.fxml"));
             Scene scene = new Scene(root);
             stage.setScene(scene);
             stage.show();
--- src/SceneController.java
+++ src/SceneController.java
@@ -1,10 +1,12 @@
 import java.io.IOException;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 
@@ -36,4 +38,55 @@ public class SceneController {
         stage.setScene(scene);
         stage.show();
     }
+    public void closeWindow(ActionEvent event)throws IOException{
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.close();
+    }
+
+    public void closeApp(ActionEvent event) {
+    Platform.exit();
+    }
+
+    public void Add_Book(ActionEvent event )throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Scene_AddBook.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.showAndWait();
+    }
+
+    public void confirmAdd_Book(ActionEvent event){
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.close();
+    }
+
+    public void Add_Doc(ActionEvent event )throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Scene_AddDoc.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.showAndWait();
+    }
+    
+    public void confirmAdd_Doc(ActionEvent event){
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.close();
+    }
+    public void Add_Mag(ActionEvent event )throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Scene_AddDoc.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.showAndWait();
+    }
+    
+    public void confirmAdd_Mag(ActionEvent event){
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.close();
+    }
+
+
 }
